9688b214c02a29ebf55a8334e4d37f2756e98e92
sandobits <sandersonbarbosa36@gmail.com>
2020-05-14 17:16:31 -0300

    Add external api request
    * Add axios
    * Add routes.js
    * Add first function

---
+++ README.md
@@ -0,0 +1,13 @@
+# Desafio Mercadou
+
+## Rodando a API
+
+```sh
+git clone https://github.com/sandobits/s4ndb4r.git
+cd s4ndb4r
+npm i && npm run dev
+```
+
+## Rodando o frontend
+
+Coming soon...
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import api from './api'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  return res.status(200).json("OK")
+router.get('/', async (req, res) => {
+  let api_res = await api.get('/api/v2/pokedex/2/')  
+  
+  console.log(api_res.data.pokemon_entries)
+  return res.status(200).json(api_res.data.pokemon_entries)
 })
 
 export default router
