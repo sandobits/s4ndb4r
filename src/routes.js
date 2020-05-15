fdbc34d25c023f14e91764637f92d7403bda5944
sandobits <sandersonbarbosa36@gmail.com>
2020-05-15 01:12:32 -0300

    Remade types route into /type/:type

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import * as PokemonController from './controllers/PokemonController'
 const router = Router()
 
 router.get('/', PokemonController.getFromKanto)
-router.get('/:type', PokemonController.getByType)
+router.get('/type/:type', PokemonController.getByType)
 router.get('/view/:poke_id', PokemonController.view)
 
 export default router
