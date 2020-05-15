87a75813d0666947ae478b0105bac2031cb481a9
sandobits <sandersonbarbosa36@gmail.com>
2020-05-15 00:24:20 -0300

    Add parameter verification on getByType function
    * Add Pokemon.view() function

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -12,7 +12,6 @@ const getFromKanto =  async (req, res) => {
 }
 
 // lista pokémons da primeira geração filtrados pelo tipo selecionado
-
 const getByType = async (req, res) => {
   let { type } = req.params
 
@@ -40,22 +39,34 @@ const getByType = async (req, res) => {
     'shadow'
   ]
   
-  // verifica se o texto passado como 'type' pelo URL é um dos 18 tipos válidos e "traduz"
-  // esse texto para o ID do tipo apropriado para realizar a request por tipo
+  // verifica se o parametro é texto, e caso seja, se está na lista de types validos
+  if (isNaN(parseInt(type))) {
+    if (!typeNames.includes(type)) {
+      return res.status(400).json(`Invalid type: ${type}`)
+    }
+  }
 
+  // verifica se o parametro (numerico) está entre 1 e 18
+  if (type > 18 || type < 1) {
+    return res.status(400).json(`Invalid type: ${type}`)
+  }
+  
+  // verifica se o ID passado como 'type' pelo URL é um dos 18 tipos válidos e "traduz"
+  // esse texto para o ID do tipo apropriado para realizar a request por tipo
+  // essas operações são sempre negativas se o parâmetro for passado como texto
   for (let i = 0; i < typeNames.length; i++) {
     if (typeNames[i] == type) {
       type = (i+1).toString()
     }
   }
+
   let external_res = await pokeapi.get(`/api/v2/type/${type}`)
   let pokemons = external_res.data.pokemon
 
   let gen1 = []
 
-  // lê todos os pokemons do array e verifica, através dos seus IDs, se eles são da gen1 (1-151)
+  // lê todos os pokemons do array e verifica, através dos seus IDs, se eles são da gen1 (1-151),
   // e os adiciona a um array de pokemons válidos
-
   for (let i = 0; i < pokemons.length; i++) {
     var url = pokemons[i].pokemon.url.slice(0, -1)
     let poke_id = url.split('/').pop()
@@ -66,7 +77,24 @@ const getByType = async (req, res) => {
       }
     }
   }
-  return res.status(200).json(gen1)
+  return res.status(200).json({type: typeNames[type-1], gen1})
 }
 
-export { getFromKanto, getByType }
+// detalha único pokémon, a partir do ID ou NOME
+const view = async (req, res) => {
+  let { poke_id } = req.params
+
+  let external_res = await pokeapi.get(`/api/v2/pokemon/${poke_id}`)
+  let pokemon = external_res.data
+  
+  let pokedata = {
+    name: pokemon.name,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    sprite: pokemon.sprites.front_default
+  }
+
+  return res.status(200).json(pokedata)
+}
+
+export { getFromKanto, getByType, view }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.get('/', PokemonController.getFromKanto)
 router.get('/:type', PokemonController.getByType)
+router.get('/view/:poke_id', PokemonController.view)
 
 export default router
