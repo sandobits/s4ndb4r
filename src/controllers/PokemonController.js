354e205c270d70fc428292b017074650cfcfe146
sandobits <sandersonbarbosa36@gmail.com>
2020-05-15 01:00:46 -0300

    Remove comments on PokemonController

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -1,13 +1,14 @@
 // tradicionalmente incluiriamos aqui os 4 métodos padrão para um recurso: GET, POST, PUT, e DELETE;
 // porém, dado o desafio, todas as requisições GET ficarão aqui temporariamente, e serão abertas em
 // outros controllers conforme necessário
+// decidi incluir detalhes sobre verificação de valores apenas uma vez, já que alguns foram feitos
+// de maneira quase idêntica em mais de um local
 import pokeapi from '../api'
 
 // lista todos os pokémons da primeira geração (Kanto)
 const getFromKanto =  async (req, res) => {
   let external_res = await pokeapi.get('/api/v2/pokedex/2/')  
   let data = external_res.data.pokemon_entries
-  
   return res.status(200).json(data)
 }
 
@@ -15,7 +16,7 @@ const getFromKanto =  async (req, res) => {
 const getByType = async (req, res) => {
   let { type } = req.params
 
-  // define os tipos válidos, na ordem apropriada
+  // define os nomes dos tipos válidos, na ordem apropriada
   let typeNames = [
     'normal',
     'fighting',
@@ -46,14 +47,12 @@ const getByType = async (req, res) => {
     }
   }
 
-  // verifica se o parametro (numerico) está entre 1 e 18
-  if (type > 18 || type < 1) {
+  // verifica se o parametro (numerico) está entre 1 e quantos tipos válidos a lista contiver, neste caso 18
+  if (type < 1 || type > typeNames.length) {
     return res.status(400).json(`Invalid type: ${type}`)
   }
-  
-  // verifica se o ID passado como 'type' pelo URL é um dos 18 tipos válidos e "traduz"
-  // esse texto para o ID do tipo apropriado para realizar a request por tipo
-  // essas operações são sempre negativas se o parâmetro for passado como texto
+
+  // transforma, invariavelmente, o texto do tipo (caso seja) para o código correspondente
   for (let i = 0; i < typeNames.length; i++) {
     if (typeNames[i] == type) {
       type = (i+1).toString()
@@ -62,7 +61,6 @@ const getByType = async (req, res) => {
 
   let external_res = await pokeapi.get(`/api/v2/type/${type}`)
   let pokemons = external_res.data.pokemon
-
   let gen1 = []
 
   // lê todos os pokemons do array e verifica, através dos seus IDs, se eles são da gen1 (1-151),
@@ -80,14 +78,23 @@ const getByType = async (req, res) => {
   return res.status(200).json({type: typeNames[type-1], gen1})
 }
 
-// detalha único pokémon, a partir do ID ou NOME
+// detalha único pokémon, a partir do ID 
 const view = async (req, res) => {
   let { poke_id } = req.params
+  
+  if (isNaN(parseInt(poke_id))){
+    return res.status(400).json(`Invalid poke_id: ${poke_id}`)
+  }
 
   let external_res = await pokeapi.get(`/api/v2/pokemon/${poke_id}`)
   let pokemon = external_res.data
-  
+
+  if (pokemon.id > 151 || pokemon.id < 1) {
+    return res.status(400).json(`Pokémon does not belong in Kanto region: ${pokemon.name}`)
+  }
+
   let pokedata = {
+    poke_id: pokemon.id,
     name: pokemon.name,
     height: pokemon.height,
     weight: pokemon.weight,
