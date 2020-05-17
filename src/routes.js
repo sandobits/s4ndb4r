f061d4936b588fcd610b5c240ac9fb5601468c15
sandobits <sandersonbarbosa36@gmail.com>
2020-05-17 12:29:31 -0300

    Add getTypes function
    * Fix api baseURL

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import * as PokemonController from './controllers/PokemonController'
+import * as TypeController from './controllers/TypeController'
 
 const router = Router()
 
 router.get('/', PokemonController.getFromKanto)
+router.get('/type/', TypeController.getTypes)
 router.get('/type/:type', PokemonController.getByType)
 router.get('/pokemon/:poke_id', PokemonController.viewSingle)
 
